526d4dbeba9d058197112a1529fc61c46314de97
Nadeem012 <55021916+ndm81@users.noreply.github.com>
2020-11-04 13:17:55 +0530

    Add files via upload

---
+++ FinalMethod.java
@@ -0,0 +1,15 @@
+class Car{
+	final void run(){
+		System.out.println("Speed of the car is");
+	}}
+   class Ferrari extends Car
+   {
+   	    void run(){
+   	    System.out.println("Speed of the car is");
+   	 }
+   	    public static void main(String[] args) {
+   		Ferrari fer=new Ferrari();
+   		        fer.run();
+   	}
+   }
+
